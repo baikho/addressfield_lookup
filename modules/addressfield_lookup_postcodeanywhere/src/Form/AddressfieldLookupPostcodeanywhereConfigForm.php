e4e2c46292b3de312f041cb1fbc657448bfd71e4
Sang Lostrie <sang.lostrie@gmail.com>
2020-04-24 00:01:06 +0100

    Minor improvements to AddressfieldLookupPostcodeanywhereConfigForm.

--- modules/addressfield_lookup_postcodeanywhere/src/Form/AddressfieldLookupPostcodeanywhereConfigForm.php
+++ modules/addressfield_lookup_postcodeanywhere/src/Form/AddressfieldLookupPostcodeanywhereConfigForm.php
@@ -1,16 +1,16 @@
 <?php
 
-/**
- * @file
- * Contains \Drupal\addressfield_lookup_postcodeanywhere\Form\AddressfieldLookupPostcodeanywhereConfigForm.
- */
-
 namespace Drupal\addressfield_lookup_postcodeanywhere\Form;
 
 use Drupal\Core\Form\ConfigFormBase;
 use Drupal\Core\Form\FormStateInterface;
 use Drupal\Core\Render\Element;
 
+/**
+ * Class AddressfieldLookupPostcodeanywhereConfigForm.
+ *
+ * @package Drupal\addressfield_lookup_postcodeanywhere\Form
+ */
 class AddressfieldLookupPostcodeanywhereConfigForm extends ConfigFormBase {
 
   /**
@@ -45,27 +45,33 @@ class AddressfieldLookupPostcodeanywhereConfigForm extends ConfigFormBase {
     return ['addressfield_lookup_postcodeanywhere.settings'];
   }
 
-  public function buildForm(array $form, \Drupal\Core\Form\FormStateInterface $form_state) {
+  /**
+   * {@inheritdoc}
+   */
+  public function buildForm(array $form, FormStateInterface $form_state) {
+
+    $config = $this->config('addressfield_lookup.settings');
+
     $form['addressfield_lookup_postcodeanywhere_login'] = [
-      '#title' => t('Login'),
+      '#title' => $this->t('Login'),
       '#type' => 'textfield',
-      '#default_value' => \Drupal::config('addressfield_lookup.settings')->get('addressfield_lookup_postcodeanywhere_login'),
-      '#description' => t('The login associated with the Royal Mail license (not required for click licenses).'),
+      '#default_value' => $config->get('addressfield_lookup_postcodeanywhere_login'),
+      '#description' => $this->t('The login associated with the Royal Mail license (not required for click licenses).'),
     ];
 
     $form['addressfield_lookup_postcodeanywhere_license'] = [
-      '#title' => t('License'),
+      '#title' => $this->t('License'),
       '#type' => 'textfield',
-      '#default_value' => \Drupal::config('addressfield_lookup.settings')->get('addressfield_lookup_postcodeanywhere_license'),
-      '#description' => t('API key to use to authenticate to Postcode Anywhere.'),
+      '#default_value' => $config->get('addressfield_lookup_postcodeanywhere_license'),
+      '#description' => $this->t('API key to use to authenticate to Postcode Anywhere.'),
     ];
 
     $form['addressfield_lookup_postcodeanywhere_country_quality'] = [
-      '#title' => t('Minimum addressing quality'),
+      '#title' => $this->t('Minimum addressing quality'),
       '#type' => 'select',
       '#options' => array_combine(range(1, 5), range(1, 5)),
-      '#default_value' => \Drupal::config('addressfield_lookup.settings')->get('addressfield_lookup_postcodeanywhere_country_quality'),
-      '#description' => t('The minimum quality value (1 least to 5 most) of address data required. This quality value is used to filter supported countries pulled from <a href="@country_url" target="_blank">PCA Predict</a>', [
+      '#default_value' => $config->get('addressfield_lookup_postcodeanywhere_country_quality'),
+      '#description' => $this->t('The minimum quality value (1 least to 5 most) of address data required. This quality value is used to filter supported countries pulled from <a href="@country_url" target="_blank">PCA Predict</a>', [
         '@country_url' => 'https://www.pcapredict.com/support/webservice/extras/lists/countrydata/3'
         ]),
       '#element_validate' => ['element_validate_integer_positive'],
@@ -77,10 +83,13 @@ class AddressfieldLookupPostcodeanywhereConfigForm extends ConfigFormBase {
     return parent::buildForm($form, $form_state);
   }
 
-  public function _submitForm(array &$form, \Drupal\Core\Form\FormStateInterface $form_state) {
+  /**
+   * {@inheritdoc}
+   */
+  public function _submitForm(array &$form, FormStateInterface $form_state) {
     // Show warning if supported countries data quality level is below 3.
     if ($form_state->getValue(['addressfield_lookup_postcodeanywhere_country_quality']) < 3) {
-      drupal_set_message(t('You have selected an addressing quality below 4. This is not recommended and could result in poor quality address lookups.'), 'warning');
+      $this->messenger()->addWarning($this->t('You have selected an addressing quality below 4. This is not recommended and could result in poor quality address lookups.'));
     }
 
     $form_state->set(['redirect'], 'admin/config/regional/addressfield-lookup');
